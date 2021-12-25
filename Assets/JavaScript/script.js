989d4b56cfc3870982f9ef36b5d278f84880af3f
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2021-12-25 14:45:07 -0500

    used same foreach function to create array/clickable for .task items

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -6,25 +6,27 @@ $("#currentDay").text(dayToday);
 //This is the variable selecting all buttons with .clear
 var clearButton = document.getElementById("timeBody").querySelectorAll(".clear"); 
 console.log (clearButton);
-
-//this method will output 'thistext' when a selected button is clicked.  I WANT IT TO WORK FOR ALL OF THEM
-clearButton[0].addEventListener ("click", function() { 
-    // TODO: make this function 'clear' the content of the associated row of the second column in the grid
-}
-)
-
+//Method(function) for making all Clear buttons DO something
 document.querySelectorAll('.clear').forEach(clearButton => {
-  clearButton.addEventListener('click', event => {console.log ("hey!")
+  clearButton.addEventListener('click', event => 
+  //TODO: THE BELOW FUNCTION WILL CLEAR OUT CONTENT OF COLUMN 2 FOR THE APPROPRIATE ROW ONCLICK
+  {console.log ("hey!")
+  })
+})
+
+var times = document.getElementById("timeBody").querySelectorAll(".task"); 
+console.log (times);
+//Method(function) for making all Clear buttons DO something
+document.querySelectorAll('.task').forEach(times => {
+  times.addEventListener('click', event => 
+  //TODO: THE BELOW FUNCTION WILL CLEAR OUT CONTENT OF COLUMN 2 FOR THE APPROPRIATE ROW ONCLICK
+  {console.log ("listen!")
   })
 })
 
 
-//THE ABOVE WILL WORK PUTTING [ARRAYSELECTION 0-24 FOR ONE BUTTON] BUT IM TRYING TO MAKE A FOR LOOP SO THAT I SELECTS ALL OF THE BUTTONS WITH THE CLEAR CLASE TO MAKE THEM ALL WORK
-// for (i=0, i <= clearButton.length, i++ ){}
-
-
-var times = ["1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12AM"];
-console.log (times.length);
+// var times = ["1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12AM"];
+// console.log (times.length);
 
 var toDo = document.getElementById('1AM');
 var myText = document.getElementById('theDiv');
