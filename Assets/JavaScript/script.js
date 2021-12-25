c49cca35509bb3ad5c108f8cec8d9daf0499f764
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2021-12-25 13:18:26 -0500

    figured out how to make multiple buttons work .clear BUT can not make for loop work so i don't have to copy/paste 24 times

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -3,15 +3,26 @@ var dayToday = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(dayToday);
 
 
-//This is the variable to make the clear button work later
-var clearButton = document.getElementById("timeBody").querySelectorAll(".clear");
-console.log (clearButton;)
+//This is the variable selecting all buttons with .clear
+var clearButton = document.getElementById("timeBody").querySelectorAll(".clear"); 
+console.log (clearButton);
 
-clearButton.addEventListener ("click", function() { 
+//this method will output 'thistext' when a selected button is clicked.  I WANT IT TO WORK FOR ALL OF THEM
+clearButton[0].addEventListener ("click", function() { 
     // TODO: make this function 'clear' the content of the associated row of the second column in the grid
-    console.log ("thistext");
+    console.log ("thistext"); 
 }
 )
+clearButton[1].addEventListener ("click", function() { 
+  // TODO: make this function 'clear' the content of the associated row of the second column in the grid
+  console.log ("thistext"); 
+}
+)
+
+
+//THE ABOVE WILL WORK PUTTING [ARRAYSELECTION 0-24 FOR ONE BUTTON] BUT IM TRYING TO MAKE A FOR LOOP SO THAT I SELECTS ALL OF THE BUTTONS WITH THE CLEAR CLASE TO MAKE THEM ALL WORK
+// for (i=0, i <= clearButton.length, i++ ){}
+
 
 var times = ["1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12AM"];
 console.log (times.length);
