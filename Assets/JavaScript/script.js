04733d0507164344d235c9e6abe0c0e85d17ecbe
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2021-12-25 12:44:27 -0500

    learning queryselector vs. queryselectorAll

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,10 +1,11 @@
+// This is the current day at the top of the page.
 var dayToday = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(dayToday);
 
-var clearButton = document.querySelector(".clear");
 
-// TODO: make a grid with 24 hours and three columns: ["time", "textcontent", "clearbutton"]
-// TODO: use some function to create that grid so that it's filled in with arrays- RESEARCH and STUDY
+//This is the variable to make the clear button work later
+var clearButton = document.getElementById("timeBody").querySelectorAll(".clear");
+console.log (clearButton;)
 
 clearButton.addEventListener ("click", function() { 
     // TODO: make this function 'clear' the content of the associated row of the second column in the grid
@@ -12,13 +13,15 @@ clearButton.addEventListener ("click", function() {
 }
 )
 
+var times = ["1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12AM"];
+console.log (times.length);
+
 var toDo = document.getElementById('1AM');
 var myText = document.getElementById('theDiv');
 
 //TODO: make taskinput be what the user writes in
 toDo.addEventListener('click', function(){
   var taskInput = 'Hello World !';
-td.1AM = taskInput;
 });
 
 
