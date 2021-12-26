d2263f86fae37dd976b96d434acb5dfc475b82f0
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2021-12-26 15:09:13 -0500

    trying to create input field onclick

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,36 +2,46 @@
 var dayToday = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(dayToday);
 
-// This is the variable selecting all of the elements .task
+// variables selecting all of the elements .task and all elements .clear
 var tasks = document.getElementById("timeBody").querySelectorAll(".task"); 
 console.log (tasks);
+var clearButton = document.getElementById("timeBody").querySelectorAll(".clear"); 
+
 //Method(function) for making all .task elements DO something
 document.querySelectorAll('.task').forEach(tasks => {
   tasks.addEventListener('click', event => 
   //TODO: THE BELOW FUNCTION WILL OPEN EDITABLE TEXT AREA ONCLICK
-  {console.log ("listen!"), tasks.textContent = "check it out!"
+  {createInputField()
   })
 })
-console.log (tasks.length);
 
-//This is the variable selecting all buttons with .clear
-var clearButton = document.getElementById("timeBody").querySelectorAll(".clear"); 
-console.log (clearButton);
+console.log (document.body.children[1].children[0].children.timeBody.children[0].children[1])
+
+//trying to put the inputfield in the block area
+function createInputField() {
+  var inputField = document.createElement("INPUT");
+  inputField.setAttribute("type", "text");
+  console.log ("myfunction worked");
+  document.body.children[1].children[0].children.timeBody.children[0].children[1].appendChild = inputField
+}
+
+
 //Method(function) for making all Clear buttons DO something
 document.querySelectorAll('.clear').forEach(clearButton => {
   clearButton.addEventListener('click', event => 
-  //TODO: THE BELOW FUNCTION WILL CLEAR OUT CONTENT OF COLUMN 2 FOR THE APPROPRIATE ROW ONCLICK
-  {console.log ("hey!")
-  })
+  //TODO: THE BELOW FUNCTION WILL CLEAR OUT CONTENT OF COLUMN 2 FOR THE APPROPRIATE ROW ONCLICK --- below is my starting to attempt and make clear button clear tasks block content
+  {console.log ("hey!") 
+  } 
+  )
 })
 
-var toDo = document.getElementById('1AM');
-var myText = document.getElementById('theDiv');
+
+
 
 //TODO: make taskinput be what the user writes in
-toDo.addEventListener('click', function(){
-  var taskInput = 'Hello World !';
-});
+// toDo.addEventListener('click', function(){
+//   var taskInput = 'Hello World !';
+// });
 
 
 //   // Creates element based on tag entered by user
